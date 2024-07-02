ef2a2816f589345de37fba4a7aadc43adfd676a7
nagata1634 <79886323+nagata1634@users.noreply.github.com>
2024-07-02 15:10:44 +0900

    [作成]クラスファイルをすべて作成しました

---
+++ src/main/java/dao/AccountDAO.java
@@ -0,0 +1,31 @@
+package dao;
+
+import java.util.List;
+
+public class AccountDAO implements Dao {
+
+	@Override
+	public List<Object> search() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public List<Object> search(Object object) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public void create(Object object) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void remove(Object object) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/main/java/model/ProductSearch.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.List;
+
+public class ProductSearch implements Search {
+
+	@Override
+	public List<Object> search(Object object) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
