22c0fc3bc3612343997ba6db0677788406e62505
nagata1634 <79886323+nagata1634@users.noreply.github.com>
2024-07-04 02:08:47 +0900

    [作成]データの格納クラスを作成しました

---
+++ src/main/java/dao/DAO.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dao;
+/**
+ *
+ * @author yuuya
+ */
+public interface DAO {
+    String JDBC_URL = "jdbc:mariadb://localhost?characterEncoding=UTF-8&serverTimezone=Asia/Tokyo";
+    String DB_USER = "root";
+    String DB_PASS = "";
+}
