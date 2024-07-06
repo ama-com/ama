7cf90dc0bdf9e734f7056ded70de510c8be98e9b
nagata1634 <79886323+nagata1634@users.noreply.github.com>
2024-07-06 10:50:04 +0900

    [Chenge]change <T>

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -3,12 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package dao;
+
+import java.util.List;
+
 /**
  *
  * @author yuuya
  */
-public interface DAO {
+public interface DAO<T> {
     String JDBC_URL = "jdbc:mariadb://localhost:3306/ama?characterEncoding=UTF-8&serverTimezone=Asia/Tokyo";
     String DB_USER = "root";
     String DB_PASS = "";
+    public List<T> searchAll();
+    public List<T> search(T obj);
+    public boolean create(T obj);
+    public boolean remove(T obj);
+    public boolean update(T obj);
 }
